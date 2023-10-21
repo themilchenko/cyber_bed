598ca259d92f7e5f69e98c4cbd5021e0e473cf19
themilchenko <ivan.milchenko.92@mail.ru>
2023-10-21 18:48:51 +0300

    BCKLG-4: Add to config new api

--- internal/plants-api/perenual.go
+++ internal/plants-api/perenual.go
@@ -2,7 +2,11 @@ package plants_api
 
 import (
 	"context"
+	"net/http"
 	"net/url"
+
+	"github.com/carlmjohnson/requests"
+	"github.com/pkg/errors"
 
 	"github.com/cyber_bed/internal/domain"
 	"github.com/cyber_bed/internal/models"
@@ -24,6 +28,23 @@ func NewPerenualAPI(url *url.URL, token string) domain.PlantsAPI {
 }
 
 func (p *PerenualAPI) SearchByName(ctx context.Context, name string) ([]models.Plant, error) {
+	u := p.baseURL
+	q := u.Query()
+	q.Set("q", name)
+
+	u.RawQuery = q.Encode()
+	apiURL := u.JoinPath("search")
+
+	var resp models.SearchSliceResponse
+
+	if err := requests.
+		URL(apiURL.String()).
+		Method(http.MethodGet).
+		ToJSON(&resp).
+		Fetch(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to search plant by name")
+	}
+
 	return nil, nil
 }
 
